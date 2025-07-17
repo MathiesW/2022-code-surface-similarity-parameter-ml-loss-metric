c4c3172ef26c3ef0e8a0f0f03e9a5e44c8bb5aa3
Mathies Wedler <mathies.wedler@dlr.de>
2025-07-17 08:30:13 +0200

    changed argument names from (y1,y2) to (x1,x2) refined docstring

--- src/ssp/numpy/ssp.py
+++ src/ssp/numpy/ssp.py
@@ -1,97 +1,102 @@
 import numpy as np
 
 
-def sobolev_norm(y):
+def sobolev_norm(x):
     """
     Sobolev / L2 norm along last axis by means of np.linalg.norm
 
     Parameters
     ----------
-    y : array_like
+    x : array_like
         Input array. Must be either 1-D or 2-D
 
     Returns
     -------
     n : float or ndarray
         Norm of the matrix or vector(s).
+
     """
-    return np.linalg.norm(y, axis=-1)
+
+    return np.linalg.norm(x, axis=-1)
 
 
-def ssp(y1, y2, batched=False):
+def ssp(x1, x2, batched=False):
     """
-    Surface Similarity Parameter (SSP) without any fuzz, cf. https://doi.org/10.1007/s10665-016-9849-7
+    Surface Similarity Parameter (SSP) without any fuzz, cf. https://doi.org/10.1007/s10665-016-9849-7.
+    This implementation does not rely in any FFT call and thus complies with n-D data.
 
     Parameters
     ----------
-    y1 : array_like
-    y2 : array_like
+    x1 : ndarray
+        First input array for comparison
+    x2 : ndarray
+        Second input array for comparison
     batched : bool, optional
-        If this is set to True, the result is not reduced along the first dimension.
+        If this is set to `True`, the result is not reduced along the first dimension.
         This option allows to use the SSP for comparing batches of data.
 
     Returns
     -------
-    y : float or ndarray
-        Surface Similarity Parameter between arrays y1 and y2.
+    ssp : ndarray
+        Surface Similarity Parameter between arrays x1 and x2.
 
     Notes
     -----
     The result is always within [0, 1] with
-        0 indicating perfect agreement, and
-        1 indicating perfect disagreement.
-    'Perfect disagreement' in terms of SSP means either
+        - 0 indicating perfect agreement, and
+        - 1 indicating perfect disagreement.
+    `Perfect disagreement` in terms of SSP means either
         - a non-zero signal is compared against a zero signal, or
-        - a signal is compared against the inverse itself, e.g., ssp(x, -x)
+        - a signal is compared against the inverse itself, e.g., `ssp(x, -x)`.
 
     Examples
     --------
     >>> from ssp.numpy import ssp
     >>> import numpy as np
     >>> np.random.seed(0)  # for deterministic results
-    >>> y1 = np.random.random((2, 32))
-    >>> y2 = np.random.random((2, 32))
-    >>> ssp(y1, y2)  # some value between 0 and 1
+    >>> x1 = np.random.random((2, 32))
+    >>> x2 = np.random.random((2, 32))
+    >>> ssp(x1, x2)  # some value between 0 and 1
     np.float64(0.35119514129237195)
-    >>> ssp(y1, y1)
+    >>> ssp(x1, x1)
     np.float64(0.0)
-    >>> ssp(y1, -y1)
+    >>> ssp(x1, -x1)
     np.float64(1.0)
-    >>> ssp(y1, np.zeros_like(y1))
+    >>> ssp(x1, np.zeros_like(x1))
     np.float64(1.0)
     
-    Use 'batched=True' to get a unique result for each signal in y1 and y2
+    Use `batched=True` to get a unique result for each signal in `x1` and `x2`
 
     >>> from ssp.numpy import ssp
     >>> import numpy as np
     >>> np.random.seed(0)  # for deterministic results
-    >>> y1 = np.random.random((2, 32))
-    >>> y2 = np.random.random((2, 32))
-    >>> ssp(y1, y2, batched=True)
+    >>> x1 = np.random.random((2, 32))
+    >>> x2 = np.random.random((2, 32))
+    >>> ssp(x1, x2, batched=True)
     array([0.34864963, 0.35827101])
-    >>> ssp(y1, y1, batched=True)
+    >>> ssp(x1, x1, batched=True)
     array([0., 0.])
-    >>> ssp(y1, -y1, batched=True)
+    >>> ssp(x1, -x1, batched=True)
     array([1., 1.])
-    >>> ssp(y1, np.zeros_like(y1), batched=True)
+    >>> ssp(x1, np.zeros_like(x1), batched=True)
     array([1., 1.])
     
     """
     
-    assert y1.shape == y2.shape, f"Shapes have to match, received {y1.shape} and {y2.shape}"
+    assert x1.shape == x2.shape, f"Shapes have to match, received {x1.shape} and {x2.shape}"
     
     if batched:
         # flatten inputs along last axis to keep batch-dimension, batch dimension is maintained
-        y1 = np.reshape(y1, (y1.shape[0], -1))
-        y2 = np.reshape(y2, (y2.shape[0], -1))
+        x1 = np.reshape(x1, (x1.shape[0], -1))
+        x2 = np.reshape(x2, (x2.shape[0], -1))
     else:
         # flatten inputs completely, result will be a float
-        y1 = np.ravel(y1)
-        y2 = np.ravel(y2)
+        x1 = np.ravel(x1)
+        x2 = np.ravel(x2)
 
     # get numerator and denuminator
-    numerator = sobolev_norm(y1 - y2)
-    denum = sobolev_norm(y1) + sobolev_norm(y2)
+    numerator = sobolev_norm(x1 - x2)
+    denum = sobolev_norm(x1) + sobolev_norm(x2)
 
     return np.divide(
         numerator,
